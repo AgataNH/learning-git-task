c6eb1d270a1e15794ddd2f1f7aff5cd017e67689
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-04-25 17:00:35 +0200

    Add loop

--- zadanie.py
+++ zadanie.py
@@ -4,3 +4,5 @@ shopping_dict = {
 }
 shopping_dict = {key: [ele.capitalize() for ele in shopping_dict[key]] for key in shopping_dict}
 new_dict = dict((key.capitalize(), value) for key, value in shopping_dict.items())
+for key, value in new_dict.items():
+    print(f"Idę do {key} i kupuję tam {value}")
