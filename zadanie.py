40d93fb1ff64b79ec3b8d31d92a558cc52b95a66
Agata Niedolistek <a.niedolistek@ibe.edu.pl>
2022-04-25 19:54:26 +0200

    Change veg

--- zadanie.py
+++ zadanie.py
@@ -1,6 +1,6 @@
 shopping_dict = {
     "piekarnia": ["chleb", "bułki", "pączek"],
-    "warzywniak": ["marchew", "seler", "rukola"]
+    "warzywniak": ["marchew", "seler", "pietruszka"]
 }
 shopping_dict = {key: [ele.capitalize() for ele in shopping_dict[key]] for key in shopping_dict}
 new_dict = dict((key.capitalize(), value) for key, value in shopping_dict.items())
